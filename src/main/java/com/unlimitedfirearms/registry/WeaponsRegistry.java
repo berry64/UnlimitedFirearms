cefaa587e9297a45914f3d3a2298b5d497a7f46e
berry64 <spamnut@yeah.net>
2018-08-06 23:20:58 +0800

    UpdateTest

--- src/main/java/com/unlimitedfirearms/registry/WeaponsRegistry.java
+++ src/main/java/com/unlimitedfirearms/registry/WeaponsRegistry.java
@@ -1,17 +1,25 @@
 package com.unlimitedfirearms.registry;
 
 import com.unlimitedfirearms.api.GunBase;
+import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WeaponsRegistry {
-    private static Map<String, GunBase> weaponsRegistry = new HashMap<String, GunBase>();
+    private static ConcurrentHashMap<String, Class<? extends  GunBase>> categories = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<ItemStack, GunBase> weaponsRegistry = new ConcurrentHashMap<>();
 
-    public static boolean registerWeapon(String name, GunBase weapon){
-        if(weaponsRegistry.containsKey(name))
+    public static boolean registerWeapon(ItemStack item, GunBase weapon){
+        if(weaponsRegistry.containsKey(item))
             return false;
-        weaponsRegistry.put(name, weapon);
+        weaponsRegistry.put(item, weapon);
+        return true;
+    }
+
+    public static boolean registerCategory(String id, Class<? extends  GunBase> clz){
+        if(categories.containsKey(id))
+            return false;
+        categories.put(id, clz);
         return true;
     }
 }
