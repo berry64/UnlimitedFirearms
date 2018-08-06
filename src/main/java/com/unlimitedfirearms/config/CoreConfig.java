ed2ffdee8a0a7c8e18b2f73eb1b13d994a7854fd
berry64 <spamnut@yeah.net>
2018-08-07 00:11:25 +0800

    UpdateTest

--- src/main/java/com/unlimitedfirearms/config/CoreConfig.java
+++ src/main/java/com/unlimitedfirearms/config/CoreConfig.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 
 public class CoreConfig {
 	private static ServerType server;
@@ -17,7 +18,7 @@ public class CoreConfig {
 	 * @param s The type of this server
 	 * @see ServerType
 	 */
-	public CoreConfig(ServerType s) {
+	public CoreConfig(ServerType s) throws FileAlreadyExistsException {
 		server = s;
 		if (server == ServerType.BUKKIT) {
 			loadBukkitConfig();
@@ -30,7 +31,7 @@ public class CoreConfig {
 		}
 	}
 
-	private static void loadBukkitConfig() {
+	private static void loadBukkitConfig() throws FileAlreadyExistsException {
 		File folder = BukkitCore.instance.getDataFolder();
 		File f = new File(folder, "config.yml");
 		if (!f.exists()) {
@@ -40,6 +41,10 @@ public class CoreConfig {
 
 		f = new File(folder, "lang.yml");
 		lang = loadCustomConf(f);
+
+		f = new File(folder+"/weapons/");
+		if((!f.exists())&& (!f.mkdirs()))
+			throw new FileAlreadyExistsException("Cannot create weapons folder");
 	}
 
 	private static YamlConfiguration loadCustomConf(File f) {
@@ -67,14 +72,31 @@ public class CoreConfig {
 		//TODO I don't think you should be here
 	}
 
+	/**
+	 * Returns the configuration file for UnlimitedFirearms Core
+	 *
+	 * @return the configuration file
+	 */
 	public static FileConfiguration getCoreConfig() {
 		return BukkitCore.instance.getConfig();
 	}
 
+	/**
+	 * Returns the language file currently in use
+	 *
+	 * @return language file
+	 */
 	public static YamlConfiguration getLang() {
 		return lang;
 	}
 
+	/**
+	 * Returns the type of server that the CoreConfig is currently running with
+	 * Allowed types are: NUKKIT, SPONGE, BUKKIT
+	 *
+	 * @return The ServerType
+	 * @see ServerType
+	 */
 	public static ServerType getServerType() {
 		return server;
 	}
